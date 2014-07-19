b3aba77c71a3d43feaecbb9e050099f67b52f1cf
Lalit Khattar <luckyk1592@gmail.com>
2014-07-19 23:39:37 +0530

    admin: add search functionality in nick status ui

--- app/client/admin/admin.js
+++ app/client/admin/admin.js
@@ -1,3 +1,21 @@
 Template.admin.created = function () {
   waartaa.admin.helpers.searchUserServers();
 }
+
+var _callbackSearch = function () {
+  var search = $('#search-message').val();
+  Session.set('user_servers_search_message');
+  waartaa.admin.helpers.searchUserServers(search);
+};
+
+Template.admin.events = {
+  'click #search-button': function () {
+    _callbackSearch();
+  },
+
+  'keyup #search-message': function (e) {
+    if (e.keyCode == 13) {
+      _callbackSearch();
+    }
+  }
+};
